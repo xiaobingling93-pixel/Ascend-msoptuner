06f2c6c665b8b53db72b88637d33da6c9d82b716
zsz_wlf <zhaishangzhao@huawei.com>
2026-04-16 16:55:22 +0800

    [Installation] 适配CI基于Tag/branch指定版本号
    Co-authored-by: zsz<zhaishangzhao@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !22 merge master into master
    
    [Installation] 适配CI基于Tag/branch指定版本号
    
    Created-by: zsz_wlf
    Commit-by: zsz
    Merged-by: ascend-robot
    Description: https://gitcode.com/Ascend/msot/issues/37
    ### 1. 修改描述
    - **修改原因：**  适配CI基于tag指定发布包版本号流程
    - **修改方案：**  适配CI基于tag指定发布包版本号流程
    - **修改内容：**  适配CI基于tag指定发布包版本号流程
    - [x] **涉及代码双合**(贴上另一个PR链接)：
    ----
    
    ### 2. 功能验证
    - [x] **功能自验截图**（请确保不体现个人信息）  
    - [ ] **冒烟是否通过**  不涉及
    ----
    
    ### 3. 代码检视
    - **要求：**
      - 合入功能代码大于 200 行，需要sig会议申报代码检视议题，并在PR中标注会议。
      - committer评估是否需要在sig会议进行代码检视。
      - 参与检视的committer人员名单与检视时间。
      - 大于 1000 行代码原则上不允许合入，需进行备案。
    - [x] **是否经过代码检视**  不涉及
    - [x] **是否具备UT测试用例看护** 不涉及
    - [x] **是否需要在sig会议中进行代码检视**不涉及
    - **检视committer人员名单与检视时间：**
    ----
    ### 4. 资料修改自检
    - **资料修改：**    不涉及
    ----
    
    See merge request: Ascend/msoptuner!22

--- build.py
+++ build.py
@@ -52,6 +52,8 @@ class BuildManager:
                                      help='Build action: omit for full build, "local" to skip dependency download, "test" to run unit tests')
         argument_parser.add_argument('-r', '--revision',
                                      help='Specify Git revision for internal dependent repo (e.g., msopcom).')
+        argument_parser.add_argument('--build-version', type=str, default=None, help='Build version for run/exe/dmg packages')
+        argument_parser.add_argument('--whl-version', type=str, default=None, help='WHL version for Python wheel packages')
         self.parsed_arguments = argument_parser.parse_args()
 
     def _execute_command(self, command_sequence, timeout_seconds=36000, cwd=None, env=None):
